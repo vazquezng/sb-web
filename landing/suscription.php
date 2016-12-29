38ce5eea4e72df9f723e56a92a67db63971d5170
Nicolas Gabriel Vazquez <vazquez.n.g@gmail.com>
2016-12-29 11:27:19 -0300

    fix and cambios en la landigns

--- landing/suscription.php
+++ landing/suscription.php
@@ -5,7 +5,7 @@
 	  $email = Trim(stripslashes($_POST['email']));
 	  $city = Trim(stripslashes($_POST['city']));
 
-	  $link = mysqli_connect('127.0.0.1', 'slambow_db', 'Sl4mb0wdb', '') or die(mysql_error());
+	  $link = mysqli_connect('localhost', 'slambow', 'Sl4mb0w', '') or die(mysql_error());
 
 	  if($link === false){
 		  die("ERROR: Error al conectar con la base de datos. " . mysqli_connect_error());
